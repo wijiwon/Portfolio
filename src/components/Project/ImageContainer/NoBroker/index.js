11c6ce652c92043606c6691b19d9bf1ff88a7390
wijiwon <dnlwldnjs5@gmail.com>
2024-03-28 16:05:10 +0900

    img 경로 수정

--- src/components/Project/ImageContainer/NoBroker/index.js
+++ src/components/Project/ImageContainer/NoBroker/index.js
@@ -5,49 +5,49 @@ const index = () => {
     <>
       <div className="w-full h-full flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_5.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_5.gif"}
           alt="NoBroker Project Image 5"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_1.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_1.gif"}
           alt="NoBroker Project Image 1"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_2.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_2.gif"}
           alt="NoBroker Project Image 2"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_3.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_3.gif"}
           alt="NoBroker Project Image 3"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_4.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_4.gif"}
           alt="NoBroker Project Image 4"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_5.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_5.gif"}
           alt="NoBroker Project Image 5"
           className="object-contain"
         />
       </div>
       <div className="w-full h-full flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NoBroker_img_1.gif"
+          src={process.env.PUBLIC_URL + "/NoBroker_img_1.gif"}
           alt="NoBroker Project Image 1"
           className="object-contain"
         />
--- src/components/Project/ImageContainer/NodeProject/index.js
+++ src/components/Project/ImageContainer/NodeProject/index.js
@@ -5,56 +5,56 @@ const index = () => {
     <>
       <div className="w-full h-full flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_6.png"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_6.png"}
           alt="NodeProject Project Image 6"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_1.gif"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_1.gif"}
           alt="NodeProject Project Image 1"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_2.gif"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_2.gif"}
           alt="NodeProject Project Image 2"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_3.gif"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_3.gif"}
           alt="NodeProject Project Image 3"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_4.png"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_4.png"}
           alt="NodeProject Project Image 4"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_5.png"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_5.png"}
           alt="NodeProject Project Image 5"
           className="object-contain"
         />
       </div>
       <div className="w-full h-auto flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_6.png"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_6.png"}
           alt="NodeProject Project Image 6"
           className="object-contain"
         />
       </div>
       <div className="w-full h-full flex-shrink-0 flex justify-center items-center">
         <img
-          src="/NodeProject_img_1.gif"
+          src={process.env.PUBLIC_URL + "/NodeProject_img_1.gif"}
           alt="NodeProject Project Image 1"
           className="object-contain"
         />
